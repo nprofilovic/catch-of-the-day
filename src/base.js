80d3e17fbb8b164a1250b8e0e08b183a1efa92c2
nprofilovic <nprofilovic@gmail.com>
2017-11-24 12:04:22 +0100

    Test data src

--- src/base.js
+++ src/base.js
@@ -4,6 +4,9 @@ const base = Rebase.createClass({
     apiKey: "AIzaSyDFmLPrdQ2QVfnCDkemxMyJyttoAZhucGs",
     authDomain: "zajedno-mozemo.firebaseapp.com",
     databaseURL: "https://zajedno-mozemo.firebaseio.com",
+    projectId: "zajedno-mozemo",
+    storageBucket: "zajedno-mozemo.appspot.com",
+    messagingSenderId: "127062674558"
 
 });
 
--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.updateFish = this.updateFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
     // getinitialState
@@ -50,6 +51,13 @@ class App extends React.Component {
     this.setState({fishes: fishes})
 
   }
+
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  } 
+
   loadSamples(){
     this.setState({
       fishes: sampleFishes
@@ -77,8 +85,17 @@ class App extends React.Component {
             }
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} params={this.props.params}/>
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+        <Order 
+          fishes={this.state.fishes} 
+          order={this.state.order} 
+          params={this.props.params} 
+        />
+        <Inventory
+          addFish={this.addFish}
+          loadSamples={this.loadSamples}
+          fishes={this.state.fishes}
+          updateFish={this.updateFish}
+        />
       </div>
     );
   }
