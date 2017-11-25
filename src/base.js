1a2b86568ce372b0b3f41e6d7b4a643b9842e4ac
nprofilovic <nprofilovic@gmail.com>
2017-11-25 15:43:39 +0100

    Final Build 1.0.2

---
+++ src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyDFmLPrdQ2QVfnCDkemxMyJyttoAZhucGs",
+    authDomain: "zajedno-mozemo.firebaseapp.com",
+    databaseURL: "https://zajedno-mozemo.firebaseio.com",
+    
+});
+
+export default base;
